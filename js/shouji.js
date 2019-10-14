6971685905e11f40e57a5d69ffce79ca621b17e6
liujiaxuan <2804044280@qq.com>
2019-10-14 12:17:09 +0800

    2019.10.14-上午

---
+++ js/shouji.js
@@ -0,0 +1,32 @@
+
+//购物车下拉菜单
+$("#gwc").onmouseenter=function(){
+	$("#gwc").style=`
+		border-left:1px solid #e7e7e7;
+		border-right:1px solid #e7e7e7;
+	`;
+	$("#dl").style=`
+		border-bottom:1px solid #e7e7e7;
+	`;
+	$("#ggdiv").style.display="block";
+}
+$("#gwc").onmouseleave=function(){
+	$("#gwc").style=`
+		border-left:none;
+		border-right:none;
+	`;
+	$("#dl").style=`
+		border-bottom:none;
+	`;
+	$("#ggdiv").style.display="none";
+}
+
+function $(str){
+	if(str[0]=="#"){
+		return document.getElementById(str.substring(1));
+	}else if(str[0]=="."){
+		return document.getElementsByClassName(str.substring(1));
+	}else{
+		return document.getElementsByTagName(str);
+	}
+}
